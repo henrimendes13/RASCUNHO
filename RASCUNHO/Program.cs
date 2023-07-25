0f4f3b8a473e084dc80dec67ab0fe335fdb786d2
Henrique Mendes <henriquemendes13@gmail.com>
2023-07-24 23:39:23 -0300

    Add project files.

---
+++ RASCUNHO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Digite a quantidade de loops para a sequência Fibonacci");
+            int n;
+            int x = 1, y = 0, z = 0;
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                z = x + y;
+                Console.WriteLine(z);
+                y = x;
+                x = z;
+            }
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
